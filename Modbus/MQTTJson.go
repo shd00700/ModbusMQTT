dd2e9fb3e1ad6250328d7b308e9184d6267f8fa1
shd00700 <64176753+shd00700@users.noreply.github.com>
2020-06-22 13:47:39 +0900

    Update MQTTJson.go

--- Modbus/MQTTJson.go
+++ Modbus/MQTTJson.go
@@ -3,8 +3,8 @@ package ModbusMQTT
 import(
 	"encoding/json"
 	MQTT "github.com/eclipse/paho.mqtt.golang"
-	
-	)
+
+)
 
 var opts = MQTT.NewClientOptions().AddBroker("tcp://broker.hivemq.com:1883")
 
@@ -13,12 +13,81 @@ type ReadCoil struct {
 	StartAdd uint16
 	AnalogState []int
 }
+
+type ReadReg struct {
+	FunctionCode string
+	StartAdd uint16
+	AnalogState []uint16
+}
+
+type WriteRegs struct {
+	FunctionCode string
+	StartAdd uint16
+	AnalogState []string
+}
+
 type ErrJson struct {
 	Errormessage string
 }
 
 func ReadCoilPublish(q byte,r bool,a uint16,b []int){
-	j := ReadCoil{"DC Current\n", a,b}
+	j := ReadCoil{"ReadCoil", a,b}
+	p, err := json.Marshal(j)
+	if err!=nil{
+		panic(err)
+	}
+
+	c := MQTT.NewClient(opts)
+	if token := c.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+	token := c.Publish("test/topic12/1",q,r,p); token.Wait()
+}
+
+func ReadCoilInPublish(q byte,r bool,a uint16,b []int){
+	j := ReadCoil{"ReadCoilIn", a,b}
+	p, err := json.Marshal(j)
+	if err!=nil{
+		panic(err)
+	}
+
+	c := MQTT.NewClient(opts)
+	if token := c.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+	token := c.Publish("test/topic12/1",q,r,p); token.Wait()
+}
+
+func ReadRegPublish(q byte,r bool,a uint16,b []uint16){
+	j := ReadReg{"ReadRegister", a,b}
+	p, err := json.Marshal(j)
+	if err!=nil{
+		panic(err)
+	}
+
+	c := MQTT.NewClient(opts)
+	if token := c.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+	token := c.Publish("test/topic12/1",q,r,p); token.Wait()
+}
+
+func ReadRegInPublish(q byte,r bool,a uint16,b []uint16){
+	j := ReadReg{"ReadInputRegister", a,b}
+	p, err := json.Marshal(j)
+	if err!=nil{
+		panic(err)
+	}
+
+	c := MQTT.NewClient(opts)
+	if token := c.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+	token := c.Publish("test/topic12/1",q,r,p); token.Wait()
+}
+
+func WriteRegsPublish(q byte,r bool,a uint16,b []string){
+	j := WriteRegs{"ReadInputRegister", a,b}
 	p, err := json.Marshal(j)
 	if err!=nil{
 		panic(err)
@@ -32,7 +101,7 @@ func ReadCoilPublish(q byte,r bool,a uint16,b []int){
 }
 
 func ErrPublish(){
-	j := ErrJson{"You entered it iolncorrectly.Return to the Output Coils menu."}
+	j := ErrJson{"You entered it iolncorrectly."}
 	p, err := json.Marshal(j)
 	if err!=nil{
 		panic(err)
