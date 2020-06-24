4dd3551694218a5e14079974eff1dea1bcd5c6be
shd00700 <64176753+shd00700@users.noreply.github.com>
2020-06-24 15:54:28 +0900

    Update MqttClient.go

--- MQTT/MqttClient.go
+++ MQTT/MqttClient.go
@@ -1,133 +1,39 @@
-package ModbusMQTT
+package main
 
-import(
-	"encoding/json"
-	MQTT "github.com/eclipse/paho.mqtt.golang"
+import (
+	"fmt"
+	"log"
+	"net/url"
+	"time"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
 
-var opts = MQTT.NewClientOptions().AddBroker("tcp://broker.hivemq.com:1883")
-
-type ReadCoil struct {
-	FunctionCode string
-	StartAdd uint16
-	DigitalState []int
-}
-type WriteCoils struct {
-	FunctionCode string
-	StartAdd uint16
-	DigitalState []string
-}
-
-type ReadReg struct {
-	FunctionCode string
-	StartAdd uint16
-	AnalogState []uint16
+func connect(clientId string, uri *url.URL) mqtt.Client {
+	opts := createClientOptions(clientId, uri)
+	client := mqtt.NewClient(opts)
+	token := client.Connect()
+	for !token.WaitTimeout(3 * time.Second) {
+	}
+	if err := token.Error(); err != nil {
+		log.Fatal(err)
+	}
+	return client
 }
 
-type WriteRegs struct {
-	FunctionCode string
-	StartAdd uint16
-	AnalogState []string
+func createClientOptions(clientId string, uri *url.URL,add string) *mqtt.ClientOptions {
+	opts := mqtt.NewClientOptions()
+	opts.AddBroker(add)
+	opts.SetUsername(uri.User.Username())
+	password, _ := uri.User.Password()
+	opts.SetPassword(password)
+	opts.SetClientID(clientId)
+	return opts
 }
 
-type ErrJson struct {
-	Errormessage string
-}
-
-func ReadCoilPublish(q byte,r bool,a uint16,b []int){
-	j := ReadCoil{"ReadCoil", a,b}
-	p, err := json.Marshal(j)
-	if err!=nil{
-		panic(err)
-	}
-
-	c := MQTT.NewClient(opts)
-	if token := c.Connect(); token.Wait() && token.Error() != nil {
-		panic(token.Error())
-	}
-	token := c.Publish("test/topic12/1",q,r,p); token.Wait()
-}
-
-func ReadCoilInPublish(q byte,r bool,a uint16,b []int){
-	j := ReadCoil{"ReadCoilIn", a,b}
-	p, err := json.Marshal(j)
-	if err!=nil{
-		panic(err)
-	}
-
-	c := MQTT.NewClient(opts)
-	if token := c.Connect(); token.Wait() && token.Error() != nil {
-		panic(token.Error())
-	}
-	token := c.Publish("test/topic12/1",q,r,p); token.Wait()
-}
-
-func ReadRegPublish(q byte,r bool,a uint16,b []uint16){
-	j := ReadReg{"ReadRegister", a,b}
-	p, err := json.Marshal(j)
-	if err!=nil{
-		panic(err)
-	}
-
-	c := MQTT.NewClient(opts)
-	if token := c.Connect(); token.Wait() && token.Error() != nil {
-		panic(token.Error())
-	}
-	token := c.Publish("test/topic12/1",q,r,p); token.Wait()
-}
-
-func ReadRegInPublish(q byte,r bool,a uint16,b []uint16){
-	j := ReadReg{"ReadInputRegister", a,b}
-	p, err := json.Marshal(j)
-	if err!=nil{
-		panic(err)
-	}
-
-	c := MQTT.NewClient(opts)
-	if token := c.Connect(); token.Wait() && token.Error() != nil {
-		panic(token.Error())
-	}
-	token := c.Publish("test/topic12/1",q,r,p); token.Wait()
-}
-
-func WriteCoilsPublish(q byte,r bool,a uint16,b []string){
-	j := WriteCoils{"WriteCoils", a,b}
-	p, err := json.Marshal(j)
-	if err!=nil{
-		panic(err)
-	}
-
-	c := MQTT.NewClient(opts)
-	if token := c.Connect(); token.Wait() && token.Error() != nil {
-		panic(token.Error())
-	}
-	token := c.Publish("test/topic12/1",q,r,p); token.Wait()
-}
-
-func WriteRegsPublish(q byte,r bool,a uint16,b []string){
-	j := WriteRegs{"WriteRegisters", a,b}
-	p, err := json.Marshal(j)
-	if err!=nil{
-		panic(err)
-	}
-
-	c := MQTT.NewClient(opts)
-	if token := c.Connect(); token.Wait() && token.Error() != nil {
-		panic(token.Error())
-	}
-	token := c.Publish("test/topic12/1",q,r,p); token.Wait()
-}
-
-func ErrPublish(){
-	j := ErrJson{"You entered it iolncorrectly."}
-	p, err := json.Marshal(j)
-	if err!=nil{
-		panic(err)
-	}
-
-	c := MQTT.NewClient(opts)
-	if token := c.Connect(); token.Wait() && token.Error() != nil {
-		panic(token.Error())
-	}
-	token := c.Publish("test/topic12/1",0,true,p); token.Wait()
+func listen(uri *url.URL, topic string) {
+	client := connect("sub", uri)
+	client.Subscribe(topic, 0, func(client mqtt.Client, msg mqtt.Message) {
+		fmt.Printf("* [%s] %s\n", msg.Topic(), string(msg.Payload()))
+	})
 }
